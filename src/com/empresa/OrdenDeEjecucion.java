cceec688d26844d89593a343f56ff470a77a2e2e
LilianaZamora <134454742+LilianaZamora@users.noreply.github.com>
2025-04-09 09:20:03 -0500

    second commit 04-09-2025 - creacion de clase OrdenDeEjecucion

---
+++ src/com/empresa/OrdenDeEjecucion.java
@@ -0,0 +1,14 @@
+package com.empresa;
+
+public class OrdenDeEjecucion {
+	public static void main(String args[]) {
+		String var1 = "¡Hola";
+		String var2 = "Equipo!";
+		int a = 100;
+		int b = 200;
+		System.out.println(var1 + " " + var2);		
+		System.out.println(a + b);	
+		System.out.println(a + b + var1 + var2);	
+		System.out.println(var1 + var2 + a + b);	
+	}
+}
