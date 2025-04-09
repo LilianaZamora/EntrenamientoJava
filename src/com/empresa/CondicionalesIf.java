697d81a0900fd237768f45a5ab534bca454830ee
LilianaZamora <134454742+LilianaZamora@users.noreply.github.com>
2025-04-09 09:50:15 -0500

    third commit 04-09-2025 - creacion de clase CondicionalesIf

---
+++ src/com/empresa/CondicionalesIf.java
@@ -0,0 +1,35 @@
+package com.empresa;
+
+public class CondicionalesIf {
+	public static void main(String args[]) {
+		String var1 = "¡Hola";
+		String var2 = "Equipo!";
+		int a = 400;
+		int b = 200;
+		
+		/*if(a>100)
+		{		
+			System.out.println("La variable a es mayor a 100");		
+		}*/		
+		/*if(a<1000)
+		{		
+			System.out.println("La variable a es menor a 1000");		
+		}
+		else {
+			System.out.println("La variable a es mayor a 1000");
+		}*/
+		if(a==100)
+		{
+			System.out.println("La variable a igual a 100");
+		} 
+		else if (a==200){
+			System.out.println("La variable a igual a 200");
+		}
+		else if (a==300){
+			System.out.println("La variable a igual a 300");
+		}
+		else {			
+				System.out.println("La variable a no es igual a ningún número");
+		}
+	}
+}
