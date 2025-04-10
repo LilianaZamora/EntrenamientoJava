531fddc16b155b7bf060572d26676bc2c99f8e8b
LilianaZamora <134454742+LilianaZamora@users.noreply.github.com>
2025-04-10 17:41:03 -0500

    third commit 04-10-2025 - creacion de clases Loops

---
+++ src/com/empresa/LoopFor.java
@@ -0,0 +1,10 @@
+package com.empresa;
+
+public class LoopFor {
+	public static void main(String args[]) {
+		
+		for(int i=1; i<=5; i++) {
+			System.out.println("Número"+i);
+		}
+	}
+}
---
+++ src/com/empresa/LoopWhile.java
@@ -0,0 +1,17 @@
+package com.empresa;
+
+public class LoopWhile {
+	public static void main(String args[]) {
+		
+		int i=1;
+		while (i<=5) {
+			System.out.println("Número"+i);
+			i++;
+		}
+		
+		/*while(true) {
+			System.err.println(i);	
+			i++;
+		}*/
+	}
+}
